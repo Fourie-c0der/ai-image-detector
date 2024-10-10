ca760cd330d0154fdde830704482c1f6fbca935e
Fourie-c0der <vanderwaltfourie6@gmail.com>
2024-10-10 10:59:31 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// ste number of tegs per page
+const tagsPerPage = 20;
+
+//event listener fo upload
+
+document.getElementById('uploadButton').addEventListener('click', async() => {
+    const fileInput = document.getElementById('imageInput');
+    const file = fileInput.files[0];
+    const imagePreview = document.getElementById('imagePreview');
+    const uploadModal = document.getElementById('uploadModal');
+})
