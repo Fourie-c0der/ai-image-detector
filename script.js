a772f549035682fc69caf1f6008111b7d17906aa
Fourie-c0der <vanderwaltfourie6@gmail.com>
2024-10-12 16:27:02 +0200

    Colors in JS file

--- script.js
+++ script.js
@@ -70,9 +70,105 @@ const tagsPerPage = 20;
             position += chunck.length;
         }
 
+        const text = new TextDecoder('utf-8').decode(responceArray);
+        const {result: { upload_id}} = JSON.parse(text);
 
+        //Get colors and tags from Imagga
+        const [colorResult, tagsResult] = await Promise.all([
+            fetch(`https://api.imagga.com/v2/colors?image_upload_id=${upload_id}`, {headers: { 'Authorization': authHeader}}).then(res => res.json()),
+            fetch(`https://api.imagga.com/v2/tags?image_upload_id=${upload_id}`, {headers: { 'Authorization': authHeader}}).then(res => res.json()),
+        ]);
 
+        //Display results
+        displayColors(colorResult.result.color);
+        displayTags(tagsResult.result.tag);
+    }catch(error){
+        console.Error('Error', error);
+        showToast('An error has occured while processing the image!');
+    }finally{
+       uploadModal.style.display = 'none'; 
     }
 }
 
 document.getElementById('uploadButton').addEventListener('click', upload);
+
+//Function to display color results
+
+const displayColors = colors =>{
+    const colorsContainer = document.querySelector('.colors-container');
+    colorsContainer.innerHTML = ''; // Clear
+
+    //If now colors are found show an error
+    if(![colors.background_colors, colors.foreground_colors, colors.image_colors].some(arr => arr.length)) {
+        
+        colorsContainer.innerHTML = '<p class="error">Nothing to show</p>';
+        return;
+    }
+
+    //Genetrate HTML sections for different color types
+    const genetrateColorSection = (title, colorData) =>{
+        return `
+
+            <h3>${title}</h3>
+            <div class="results">
+                ${colorData.map(({html_code, closest_palete_color, percent }) => 
+                `
+                    <div class="result-item" data-color="${html_code}">
+                       <div>
+                            <div class="color-box" style="background-color:${html_code}" title="Color code: ${html_code}"></div> 
+                            <p>${html_code}<span> - ${closest_palete_color}</span></p>
+                        </div>
+                        <div class="progress-bar">
+                            <span>${percent.toFixed(2)}%</span>
+                            <div class="progress" style="width: ${percent}%"></div>
+                        </div>
+                    </div>
+                `).join('')}
+            </div>
+        `;
+    };
+
+    //Append Section to the container
+    colorsContainer.innerHTML += genetrateColorSection('Background Colors', colors.background_colors);
+    colorsContainer.innerHTML += genetrateColorSection('Foreground Colors', colors.foreground_colors);
+    colorsContainer.innerHTML += genetrateColorSection('Image Colors', colors.image_colors);
+
+    document.querySelectorAll('.colors-container .result-item').forEach(item =>{
+        item.addEventListener('click', ()=>{
+            const colorCode = item.getAttribute('data-color');
+            navigator.clipboard.writeText(colorCode).then(() => showToast(`Copied: ${colorCode}`)).catch(() => showToast('Failed to copy'));
+        });
+    });
+
+};
+
+//Function to display tags
+let allTags = [];
+let displayedTags = 0;
+
+function displayTags(tags){
+    const tagsContainer = document.querySelector('.tags-container');
+    const resultList = tagsContainer.querySelector('.results');
+    const error = tagsContainer.querySelector('.error');
+    const seeMoreButton = document.querySelector('.seeMoreButton');
+    const exportTagsButton = document.querySelector('.exportTagsButton');
+
+    //clear previous tags
+    if (resultList) {
+        resultList.innerHTML = '';
+    }else{
+        const resultListContainer = document.createElement('div');
+        resultListContainer.className ='results';
+        tagsContainer.insertBefore(resultListContainer, seeMoreButton);
+    }
+
+    //Store all tags and init display tags count
+    allTags = tags;
+    displayedTags = 0;
+
+    //Function to show more tags when "See More" button is clicked
+    const showMoreTags = () =>{
+        const tagsToShow = allTags.slice(displayTags, displayTags + tagsPerPage);
+    }
+
+}
